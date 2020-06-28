e341cb1af1e75cd377bef10a9b4c41bf729907a1
Darren Mascioli <dmascioli1223@gmail.com>
2020-06-27 23:08:49 -0400

    added calendar test to home page

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 from config import Config
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models
